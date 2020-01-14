0895551894ebd9869b34d55cbe7f16e2243155cc
manuk <mahhov1@gmail.com>
2020-01-14 15:39:35 -0800

    idk. create input set and integrate input set, input params, and inputs. keeping inputs updated is confusing. will refactor to a less performant clear-then-rerender-all flow.

--- src/xElements/inputSet/InputSet.js
+++ src/xElements/inputSet/InputSet.js
@@ -3,7 +3,7 @@ const {template, name} = importUtil(__filename);
 
 customElements.define(name, class extends XElement {
 	static get attributeTypes() {
-		return {property: {}, weight: {}, filter: {}, locked: {boolean: true}};
+		return {name: {}, active: {boolean: true}}
 	}
 
 	static get htmlTemplate() {
@@ -11,47 +11,22 @@ customElements.define(name, class extends XElement {
 	}
 
 	connectedCallback() {
-		this.$('#property').addEventListener('change', () => {
-			this.property = this.$('#property').value;
-			this.emit('change');
+		this.$('#name').addEventListener('change', () => this.emit('name-change'));
+		this.$('#active').addEventListener('input', () => {
+			this.active = this.$('#active').checked;
+			this.emit('active-change');
 		});
-		this.$('#weight').addEventListener('input', () => {
-			this.weight = this.$('#weight').value;
-			this.emit('change');
+		this.$('#remove').addEventListener('click', e => {
+			this.emit('remove');
+			e.stopPropagation();
 		});
-		this.$('#filter').autocompletes = ['weight', 'and', 'not'];
-		this.$('#filter').addEventListener('change', () => {
-			this.filter = this.$('#filter').value;
-			this.emit('change');
-		});
-		this.$('#lock-above').addEventListener('input', () => {
-			this.locked = this.$('#lock-above').checked;
-			this.emit('lock-change');
-		});
-		this.$('#remove').addEventListener('click', () => this.emit('remove'));
 	}
 
-	get empty() {
-		return !this.property && !this.weight;
+	set name(value) {
+		this.$('#name').value = value
 	}
 
-	set properties(value) {
-		this.$('#property').autocompletes = value;
-	}
-
-	set property(value) {
-		this.$('#property').value = value;
-	}
-
-	set weight(value) {
-		this.$('#weight').value = value;
-	}
-
-	set filter(value) {
-		this.$('#filter').value = value;
-	}
-
-	set locked(value) {
-		this.$('#lock-above').checked = value;
+	set active(value) {
+		this.$('#active').checked = value;
 	}
 });
--- src/xElements/inputs/Inputs.js
+++ src/xElements/inputs/Inputs.js
@@ -13,13 +13,88 @@ customElements.define(name, class Inputs extends XElement {
 
 	connectedCallback() {
 		this.inputSetIndex = parseInt(localStorage.getItem('input-set-index')) || 0;
-		this.inputSets = JSON.parse(localStorage.getItem('input-sets')) || [];
-		this.$('#submit-button').addEventListener('click', () => this.emit('submit'));
-		this.$('#input-params').loadQueryParams(this.inputSets[this.inputSetsIndex]);
-		this.$('#input-params').addEventListener('change', () => {
-			this.inputSets[this.inputSetIndex] = this.$('#input-params').queryParams;
-			localStorage.setItem('input-sets', JSON.stringify(this.inputSets));
+		this.inputSets = JSON.parse(localStorage.getItem('input-sets')) || [{}];
+
+		this.$('#input-set-list').addEventListener('arrange', () => {
+			this.checkLocksAndEmptyQueryProperty();
+			this.updateQueryParams();
+			// todo
 		});
+		this.$('#add-input-set-button').addEventListener('click', e => {
+			this.inputSetIndex = this.inputSets.push({});
+			let inputSetEl = this.addQuerySetEl();
+			// todo extract below 6 duplicate lines
+			let indexSetEls = [...this.$('#input-set-list').children];
+			if (!e.ctrlKey)
+				indexSetEls.forEach(indexSetEl => indexSetEl.active = false);
+			inputSetEl.active = true;
+			this.inputSetIndex = indexSetEls.indexOf(inputSetEl);
+			this.loadSelectedInputSet();
+		});
+		this.$('#input-params').addEventListener('change', () => {
+			this.inputSets[this.inputSetIndex].queryParams = this.$('#input-params').queryParams;
+			this.store();
+		});
+		this.$('#submit-button').addEventListener('click', () => this.emit('submit'));
+
+		this.inputSets.forEach(inputSet => {
+			let inputSetEl = this.addQuerySetEl();
+			inputSetEl.name = inputSet.name;
+			inputSetEl.active = inputSet.active;
+		});
+		this.loadSelectedInputSet();
+	}
+
+	addQuerySetEl() {
+		let inputSetEl = document.createElement('x-input-set');
+		inputSetEl.slot = 'list';
+		inputSetEl.name = '';
+		this.$('#input-set-list').appendChild(inputSetEl);
+		inputSetEl.addEventListener('click', e => {
+			let indexSetEls = [...this.$('#input-set-list').children];
+			if (!e.ctrlKey)
+				indexSetEls.forEach(indexSetEl => indexSetEl.active = false);
+			inputSetEl.active = true;
+			this.inputSetIndex = indexSetEls.indexOf(inputSetEl);
+			this.loadSelectedInputSet();
+			this.store();
+		});
+		inputSetEl.addEventListener('name-change', () => {
+			let index = [...this.$('#input-set-list').children].indexOf(inputSetEl);
+			this.inputSets[this.inputSetIndex].name = inputSetEl.name;
+			this.store();
+		});
+		inputSetEl.addEventListener('active-change', () => {
+			let index = [...this.$('#input-set-list').children].indexOf(inputSetEl);
+			this.inputSets[this.inputSetIndex].active = inputSetEl.active;
+			this.store();
+		});
+		inputSetEl.addEventListener('remove', () => {
+			let inputSetEls = [...this.$('#input-set-list').children];
+			let index = inputSetEls.indexOf(inputSetEl);
+			if (this.inputSetIndex >= index)
+				this.inputSetIndex--;
+			this.inputSets.splice(index, 1);
+			if (!this.inputSets.length) {
+				this.inputSets.push({});
+				this.inputSetIndex = 0;
+				let inputSetEl = this.addQuerySetEl();
+				inputSetEl.active = true;
+			}
+			inputSetEl.remove();
+			inputSetEls[this.inputSetIndex].active = true;
+			this.store();
+		});
+		return inputSetEl;
+	}
+
+	loadSelectedInputSet() {
+		this.$('#input-params').loadQueryParams(this.inputSets[this.inputSetIndex].queryParams);
+	}
+
+	store() {
+		localStorage.setItem('input-set-index', this.inputSetIndex);
+		localStorage.setItem('input-sets', JSON.stringify(this.inputSets));
 	}
 
 	get query() {
