64f076315954439ec2759ea7751fe9f87169a652
manuk <mahhov1@gmail.com>
2020-01-17 18:49:05 -0800

    chart top and bottom corners

--- src/ItemsData.js
+++ src/ItemsData.js
@@ -17,11 +17,12 @@ class ItemsData {
 			.sort((a, b) => b.evalValue - a.evalValue || a.evalPrice - b.evalPrice);
 
 		// todo use actual max price query param instead of maximum price of items
-		let maxPrice = Math.max(...items.map(item => item.evalPrice));
+		this.maxPrice = Math.max(...items.map(item => item.evalPrice));
 		let minValue = Math.min(...items.map(item => item.evalValue).filter(v => v > 0));
 
 		// update bestBoundItems
 		let minPriceFound = Infinity;
+		// ordered top right to bottom left
 		this.bestBoundItems = this.items
 			.filter(item => {
 				if (item.evalPrice >= minPriceFound)
@@ -31,8 +32,9 @@ class ItemsData {
 			});
 
 		// update searchBoundItems
-		this.searchBoundItems.push({evalPrice: maxPrice, evalValue: minValue});
+		this.searchBoundItems.push({evalPrice: this.maxPrice, evalValue: minValue});
 		let maxPriceFound = -Infinity;
+		// ordered bottom left to top right
 		this.searchBoundItems = this.searchBoundItems
 			.sort((a, b) => a.evalValue - b.evalValue || b.evalPrice - a.evalPrice)
 			// low to high value, high to low price
@@ -44,14 +46,19 @@ class ItemsData {
 			});
 	}
 
-	get cornerPath() {
-		let path = this.searchBoundItems.flatMap(({evalValue, evalPrice}, i, a) => {
-			return [{evalValue, evalPrice: i ? a[i - 1].evalPrice : 0}, {evalValue, evalPrice}];
-		});
+	get bestBoundPath() {
+		let path = this.bestBoundItems.flatMap(({evalValue, evalPrice}, i, a) =>
+			[{evalValue, evalPrice: i ? a[i - 1].evalPrice : this.maxPrice}, {evalValue, evalPrice}]);
+		return ItemsData.itemsToPoints(path);
+	}
+
+	get searchBoundPath() {
+		let path = this.searchBoundItems.flatMap(({evalValue, evalPrice}, i, a) =>
+			[{evalValue, evalPrice: i ? a[i - 1].evalPrice : 0}, {evalValue, evalPrice}]);
 		let last = path[path.length - 1];
 		path.push({evalValue: Infinity, evalPrice: last.evalPrice});
 		path.push({evalValue: Infinity, evalPrice: 0});
-		return path;
+		return ItemsData.itemsToPoints(path);
 	}
 
 	get length() {
